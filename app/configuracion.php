3b4b2fe8fa6bc8c1fad9ca95f7cfd33e2e7b7e91
fadrian06 <franyeradriansanchez@gmail.com>
2024-03-16 20:32:34 -0400

    Centralizada la conexión a la base de datos

--- app/configuracion.php
+++ app/configuracion.php
@@ -2,12 +2,26 @@
 
 use Leaf\Auth;
 use Leaf\BareUI;
+use Leaf\Db;
 use Leaf\Router;
 use Symfony\Component\Dotenv\Dotenv;
 
 (new Dotenv())->load(__DIR__ . '/../.env');
 
 $basePath = str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']);
+
+function db(): Db {
+  static $db = null;
+
+  if (!$db) {
+    $dsn = "{$_ENV['DB_CONNECTION']}:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_DATABASE']};charset=utf8";
+    $pdo = new PDO($dsn, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+    $db = new Db;
+    $db->connection($pdo);
+  }
+
+  return $db;
+}
 
 Auth::config('ID_KEY', 'id');
 Auth::config('PASSWORD_KEY', 'clave');
@@ -16,10 +30,7 @@ Auth::config('GUARD_LOGIN', "$basePath/ingreso");
 Auth::config('GUARD_REGISTER', "$basePath/registrate");
 Auth::config('GUARD_HOME', $basePath);
 Auth::config('GUARD_LOGOUT', "$basePath/salir");
-
-$dsn = "{$_ENV['DB_CONNECTION']}:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_DATABASE']};charset=utf8";
-
-Auth::dbConnection(new PDO($dsn, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']));
+Auth::dbConnection(db()->connection());
 
 BareUI::config('path', __DIR__ . '/../vistas');
 Router::setBasePath($basePath);
