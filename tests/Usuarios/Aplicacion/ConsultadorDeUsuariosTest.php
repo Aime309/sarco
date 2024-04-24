4e41cbd97534dccf8dca32483fb26308a8e9a845
fadrian06 <franyeradriansanchez@gmail.com>
2024-04-24 18:26:57 -0400

    Extraídas configuraciones distribuidas de los tests

--- tests/Usuarios/Aplicacion/ConsultadorDeUsuariosTest.php
+++ tests/Usuarios/Aplicacion/ConsultadorDeUsuariosTest.php
@@ -11,11 +11,14 @@ use SARCOV2\Usuarios\Infraestructura\RepositorioDeUsuariosPDO;
 final class ConsultadorDeUsuariosTest extends TestCase {
   #[Test]
   function puede_consultar_usuarios(): void {
-    $consultador = new ConsultadorDeUsuarios(new RepositorioDeUsuariosPDO(new PDO(
-      'mysql:host=localhost;dbname=sarco2;charset=utf8',
+    $pdo = new PDO(
+      "mysql:host=localhost;dbname=sarco2;charset=utf8",
       'root'
-    )));
+    );
 
-    self::assertCount(11, $consultador->obtenerTodos());
+    $consultador = new ConsultadorDeUsuarios(new RepositorioDeUsuariosPDO($pdo));
+    $esperado = $pdo->query('SELECT COUNT(id) FROM usuarios')->fetchColumn();
+
+    self::assertCount($esperado, $consultador->obtenerTodos());
   }
 }
