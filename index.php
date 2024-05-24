9eab6d7db2fd31537d9d14e25e8aa02018bdf69a
Michell Manzanillo <luxiamber@gmail.com>
2024-05-24 16:52:41 -0400

    Controlador error cuando no hay dependencias

--- index.php
+++ index.php
@@ -4,7 +4,17 @@ use SARCO\App;
 
 const ROOT = __DIR__;
 
-require __DIR__ . '/vendor/autoload.php';
+try {
+  @require __DIR__ . '/vendor/autoload.php';
+} catch (Error) {
+  exit(<<<html
+  Falla al cargar el archivo
+  <code>/vendor/autoload.php</code>.
+  Ejecuta el comando
+  <code>composer install</code>
+  html);
+}
+
 require __DIR__ . '/app/configuraciones/variables de entorno.php';
 require __DIR__ . '/app/configuraciones/fechas.php';
 require __DIR__ . '/app/configuraciones/errores.php';
