ad2b892173759d0c8a10d148d172dd6a576fc826
Michell Manzanillo <luxiamber@gmail.com>
2024-07-02 13:16:53 -0400

    Añadido el filtro y ordenamiento de la tabla boletines

--- vistas/paginas/boletines/listado.php
+++ vistas/paginas/boletines/listado.php
@@ -14,21 +14,37 @@ $boletines = (fn (Boletin ...$boletines) => $boletines)(...$boletines);
 </div>
 
 <div class="container-fluid">
+  <input class="form-control mb-3" placeholder="Buscar estudiante..." oninput="w3.filterHTML('#boletines', '.boletin', this.value)">
   <div class="table-responsive">
-    <table class="table table-dark table-sm">
+    <table class="table table-dark table-sm" id="boletines">
       <thead>
         <tr class="text-center roboto-medium">
-          <th>Momento</th>
-          <th>Estudiante</th>
-          <th>Inasistencias</th>
-          <th>Proyecto</th>
-          <th>Fecha de registro</th>
+          <th onclick="w3.sortHTML('#boletines', '.boletin', 'td:nth-child(1)')">
+            Momento
+            <i class="fa fa-sort"></i>
+          </th>
+          <th onclick="w3.sortHTML('#boletines', '.boletin', 'td:nth-child(2)')">
+            Estudiante
+            <i class="fa fa-sort"></i>
+          </th>
+          <th onclick="w3.sortHTML('#boletines', '.boletin', 'td:nth-child(3)')">
+            Inasistencias
+            <i class="fa fa-sort"></i>
+          </th>
+          <th onclick="w3.sortHTML('#boletines', '.boletin', 'td:nth-child(4)')">
+            Proyecto
+            <i class="fa fa-sort"></i>
+          </th>
+          <th onclick="w3.sortHTML('#boletines', '.boletin', 'td:nth-child(5)')">
+            Fecha de registro
+            <i class="fa fa-sort"></i>
+          </th>
           <th></th>
         </tr>
       </thead>
       <tbody>
         <?php foreach ($boletines as $boletin) : ?>
-          <tr class="text-center">
+          <tr class="text-center boletin">
             <td><?= $boletin->momento() ?></td>
             <td><?= $boletin->estudiante() ?></td>
             <td><?= $boletin->inasistencias ?></td>
